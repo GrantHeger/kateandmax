cbadb7ef3c37cf44480357a45b60dfd84f6d5909
grantheger <grant@rideamigos.com>
2014-02-12 11:56:38 -0500

    pages commit

---
+++ app/contact/contact.js
@@ -0,0 +1,33 @@
+﻿(function () {
+    'use strict';
+    var controllerId = 'contact';
+    angular.module('app').controller(controllerId, ['common','datacontext', contact]);
+
+    function contact(common, datacontext) {
+        var getLogFn = common.logger.getLogFn;
+        var log = getLogFn(controllerId);
+
+        var vm = this;
+        vm.restaurants = [];
+        vm.places = [];
+        vm.title = 'Our Story';
+
+        activate();
+
+        function activate() {
+            var promises = [getRestaurants(), getPlaces ()];
+            common.activateController([], controllerId)
+                .then(function () { log('Have questions? Email me!'); });
+        }
+        function getRestaurants() {
+            return datacontext.getRestaurants().then(function (data) {
+                return vm.restaurants = data;
+            });
+        }
+        function getPlaces() {
+            return datacontext.getPlaces().then(function (data) {
+                return vm.places = data;
+            });
+        }
+    }
+})();
---
+++ obj/Release/Package/PackageTmp/app/attractions/attractions.js
@@ -0,0 +1,33 @@
+﻿(function () {
+    'use strict';
+    var controllerId = 'attractions';
+    angular.module('app').controller(controllerId, ['common','datacontext', attractions]);
+
+    function attractions(common, datacontext) {
+        var getLogFn = common.logger.getLogFn;
+        var log = getLogFn(controllerId);
+
+        var vm = this;
+        vm.restaurants = [];
+        vm.places = [];
+        vm.title = 'Our Story';
+
+        activate();
+
+        function activate() {
+            var promises = [getRestaurants(), getPlaces ()];
+            common.activateController([], controllerId)
+                .then(function () { log('Taking a look at things to do'); });
+        }
+        function getRestaurants() {
+            return datacontext.getRestaurants().then(function (data) {
+                return vm.restaurants = data;
+            });
+        }
+        function getPlaces() {
+            return datacontext.getPlaces().then(function (data) {
+                return vm.places = data;
+            });
+        }
+    }
+})();
